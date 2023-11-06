b205ec317e14e9afcd2432dc2d83cb79cbf67111
Ana Silveira <ana.ccord@gmail.com>
2023-11-06 16:16:52 -0300

    chore: adding new routes for notes  handling

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,13 @@
 const { Router } = require("express");
 
 const usersRouter = require("./users.routes")
+const notesRouter = require("./notes.routes")
 
 const routes = Router();
 
 // esse arquivo reune todas as rotas
 
 routes.use("/users", usersRouter);
+routes.use("/notes", notesRouter);
 
 module.exports = routes;
